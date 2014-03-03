59194b2a2039952e7a22903f1ebdf601c3e20d47
Cam Saul <cam@getluckybird.com>
2014-03-03 14:40:40 -0800

    GameSprite ivars

--- Classes/Abstract/GameSprite.h
+++ Classes/Abstract/GameSprite.h
@@ -9,15 +9,16 @@
 #ifndef WONKYBIRD_GAMESPRITE_H
 #define WONKYBIRD_GAMESPRITE_H
 
-#include <cocos2d.h>
-
-using namespace cocos2d;
-using namespace std;
-
 class GameSprite : public CCSprite {
 public:
 	GameSprite();
+	
+	b2Vec2 PositionForBox2D() const;
 private:
+	b2Body *body_;
+	b2BodyDef bodyDef_;
+	b2PolygonShape shape_;
+	b2FixtureDef fixture_;
 };
 
 #endif
